ca88e2ffc41c2c5cd6808df9a17c040a82abea29
Zhao Shenyang <dev@zsy.im>
2020-06-02 22:13:26 +0800

    initial commit

---
+++ __main__.py
@@ -0,0 +1,57 @@
+import time
+from oled import OLEDCtrl, SHUTDOWN
+
+def zero_f(ctrl):
+    ctrl.draw_bytes(ctrl.splash)
+    ctrl.display_refresh_time = time.time() + ctrl.display_off_timeout
+
+def one_f(ctrl):
+    ctrl.clear_lines()
+    ctrl.putline('1 pressed')
+    ctrl.putline('1 pressed', inverted=True)
+    ctrl.display_flush()
+
+def two_f(ctrl):
+    ctrl.clear_lines()
+    ctrl.putline('2 pressed')
+    ctrl.putline('2 pressed', inverted=True)
+    ctrl.display_flush()
+
+def three_f(ctrl):
+    ctrl.clear_lines()
+    ctrl.putline('No', inverted=True)
+    ctrl.putline('Yes')
+    ctrl.putline('F3: Toggle Choices')
+    ctrl.display_flush()
+
+def shutdown_f(ctrl):
+    ctrl.loop_break = True
+
+def four_f(ctrl):
+    ctrl.clear_lines()
+    ctrl.putline('No')
+    ctrl.putline('Yes', inverted=True)
+    ctrl.putline('F3: Toggle Choices')
+    ctrl.display_flush()
+
+def main():
+    state2keymap = {}
+    state2keymap[0] = [1, 2, 3]
+    state2keymap[1] = [0, 2, 3]
+    state2keymap[2] = [1, 0, 3]
+    state2keymap[3] = [1, 2, 4]
+    state2keymap[4] = [SHUTDOWN, 4, 3]
+    
+    state2cmd = {}
+    state2cmd[0] = zero_f
+    state2cmd[1] = one_f
+    state2cmd[2] = two_f
+    state2cmd[3] = three_f
+    state2cmd[4] = four_f
+
+    state2cmd[SHUTDOWN] = shutdown_f
+
+    main_ctrl = OLEDCtrl(state2keymap, state2cmd)
+
+if __name__ == '__main__':
+    main()
---
+++ oled.py
@@ -0,0 +1,217 @@
+import os
+import random
+import smbus2 as smbus
+import subprocess
+import time
+
+from fonts import normal_font, inverted_font, unknown_char
+
+KEY2PIN = {
+    1: 0,
+    2: 2,
+    3: 3,
+}
+
+SHUTDOWN = 99
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+
+class OLEDCtrl(object):
+
+    def __init__(self, state2keymap, state2cmd,
+                 key2pin=KEY2PIN, keys=(1, 2, 3),
+                 bus_num=0, polling_interval=0.05, display_off_timeout=30.0,
+                 line_length=16, line_num=8):
+
+        self.state2keymap = state2keymap
+        self.state2cmd = state2cmd
+        self.keys = keys
+        self.key2pin = key2pin
+        self.bus = smbus.SMBus(bus_num)
+        self.polling_interval = polling_interval
+        self.display_off_timeout = display_off_timeout
+
+        self.loop_break = False
+        self.display_refresh_time = 0
+        self.extent_display_off_time()
+
+        self.display_already_off = False
+
+        self.state = 0
+        self.lines = {}
+
+        self.splash = []
+        self.gen_random_splash()
+
+    def init_setup(self):
+
+        # initialize GPIO
+        with open('/sys/class/gpio/export') as f:
+            for key in self.keys:
+                pin = self.key2pin[key]
+                f.write('%d\n' % pin)
+            
+        tmpl = '/sys/class/gpio/gpio%d/direction'
+        for key in self.keys:
+            pin = self.key2pin[key]
+            path = tmpl % pin
+            with open(path, 'w') as f:
+                f.write('in\n')
+
+        self.write_i2c_block_data(0x3c, 0x00, [
+            0xae,       # set display off
+            0x00,       # set lower column address
+            0x10,       # set higher column address
+            0x40,       # set display start line
+            0xb0, 0x81, # set page address
+            0xcf,       # set screen flip
+            0xa1,       # set segment remap
+            0xa8,       # set multiplex ratio
+            0x3f,       # set duty 1/64
+            0xc8,       # set com scan direction
+            0xd3, 0x00, # set display offset
+            0xd5, 0x80, # set osc division
+            0xd9, 0xf1, # set pre-charge period
+            0xda, 0x12, # set com pins
+            0xdb, 0x40, # set vcomh
+            0x8d, 0x14, # set charge pump on
+            0xa6,       # set display normal (not inverse)
+            0x20, 0x00, # set horizontal addressing mode
+            0xaf        # set display on
+        ])
+
+    def loop(self):
+
+        while not self.loop_break:
+            time.sleep(self.polling_interval)
+
+            if keydown(1):
+                continue
+
+            if keydown(2):
+                continue
+
+            if keydown(3):
+                continue
+
+            # not here until all key are released
+            current_time = time.time()
+
+            if current_time > self.display_off_time:
+                self.display_off()
+                continue
+ 
+            elif current_time > self.display_refresh_time:
+                self.display_on()
+                self.state2cmd[self.state](self)
+
+    def cleanup(self):
+
+        self.display_off()
+
+        # release GPIO
+        with open('/sys/class/gpio/unexport', 'w') as f:
+            for key in self.keys:
+                f.write('%d\n' % key)
+
+        if self.state == SHUTDOWN:
+            # os.system('shutdown now')
+            print('shutdown!')
+            exit(0)
+        else:
+            exit(0)
+
+    def run(self):
+        try:
+            self.init_setup()
+            self.loop()
+
+        except KeyboardInterrupt:
+            print(' CTRL+C detected')
+
+        finally:
+            slef.cleanup()
+
+    def keydown(self, idx):
+        dev_tmpl = '/sys/class/gpio/gpio%d/value'
+        pin = self.key2pin[idx]
+        dev = dev_tmpl % pin
+        with open(dev) as f:
+            if f.read(1) == '1':
+                keymap = self.state2keymap[self.state]
+                self.state = keymap[idx - 1]
+                self.display_refresh_time = 0
+                self.extent_display_off_time()
+                return True
+            else:
+                return False
+
+    def putline(self, line, pos=None, inverted=False):
+        if not pos:
+            if self.lines():
+                pos = max(self.lines.keys()) + 1
+            else:
+                pos = 0
+
+        if pos >= self.line_num:
+            return
+
+        char_num = len(line)
+        diff = self.line_length - char_num
+        if diff > 0:
+            line = line + ' ' * diff
+        else:
+            line = line[self.line_length]
+
+        if inverted:
+            font = inverted_font
+        else:
+            font = normal_font
+
+        line_bs = [b for c in line for b in font.get(c, unknown_char)]
+
+        self.lines[pos] = line_bs
+
+    def display_flush(self):
+        space = normal_font[' ']
+        empty_line = space * self.line_length
+
+        bs = [b for idx in range(self.line_num)
+              for b in self.lines.get(idx, empty_line)]
+
+        self.draw_bytes(self, bs)
+
+    def clear_lines(self):
+        for i in range(self.line_num):
+            del(self.lines[i])
+
+    def display_off(self):
+        if not self.display_already_off:
+            self.bus.write_i2c_block_data(0x3c, 0x00, [0xae])
+            self.gen_random_splash()
+            self.display_already_off = True
+
+    def display_on(self):
+        self.bus.write_i2c_block_data(0x3c, 0x00, [0xaf])
+
+    def draw_bytes(self, bs):
+        blocks = chunks(bs, 32)
+        for block in blocks:
+            self.bus.write_i2c_block_data(0x3c, 0x40, block)
+
+    def gen_random_splash(self):
+        byte_num = int(128 * 64 / 8)
+        self.splash = [random.randrange(256) for i in byte_num]
+
+    def extent_display_off_time(self, base_time=None, timeout=None):
+        if not base_time:
+            base_time = time.time()
+
+        if not timeout:
+            timeout = self.display_off_timeout
+
+        self.display_off_time = base_time + timeout
